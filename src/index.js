e361294890f8517bf7d41cc85b1ad93dfffbcb2d
litvinenkoiryna <litvinenkoiryna@gmail.com>
2022-02-17 23:15:33 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+ import './scss/style.scss';
+
+const heading = document.createElement('h1');
+heading.textContent = 'It is interesting!';
+
+const root = document.querySelector('#root');
+root.append(heading);
